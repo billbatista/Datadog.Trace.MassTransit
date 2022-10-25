97f9b24133c88adf28c61da699efb93c73cbbc69
tristan <t@mrtristan.com>
2022-10-25 18:26:48 -0400

    resolution for "Value of type 'UInt64' cannot appear as table value"

--- Source/Datadog.Trace.MassTransit/MassTransitMessageHeaderHelper.cs
+++ Source/Datadog.Trace.MassTransit/MassTransitMessageHeaderHelper.cs
@@ -6,14 +6,28 @@ namespace Datadog.Trace.MassTransit
     {
         internal static void SetHeaders(PublishContext context, ulong? parentSpanId, ulong? traceId)
         {
-            context.Headers.Set(MassTransitMessageHeader.TraceId, traceId);
-            context.Headers.Set(MassTransitMessageHeader.ParentSpanId, parentSpanId);
+            if (traceId != null)
+            {
+                context.Headers.Set(MassTransitMessageHeader.TraceId, traceId.ToString());
+            }
+
+            if (parentSpanId != null)
+            {
+                context.Headers.Set(MassTransitMessageHeader.ParentSpanId, parentSpanId.ToString());
+            }
         }
 
         internal static void SetHeaders(SendContext context, ulong? parentSpanId, ulong? traceId)
         {
-            context.Headers.Set(MassTransitMessageHeader.TraceId, traceId);
-            context.Headers.Set(MassTransitMessageHeader.ParentSpanId, parentSpanId);
+            if (traceId != null)
+            {
+                context.Headers.Set(MassTransitMessageHeader.TraceId, traceId.ToString());
+            }
+
+            if (parentSpanId != null)
+            {
+                context.Headers.Set(MassTransitMessageHeader.ParentSpanId, parentSpanId.ToString());
+            }
         }
     }
 }
